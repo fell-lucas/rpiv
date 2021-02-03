129c3a92d389809729a9c6bedf642aca55f4a7ad
Samuel Modesto <samuelmodestoes@gmail.com>
2021-02-02 23:00:10 -0300

    RPIV-02  packages controller, service and repository

---
+++ imobiliaria/src/main/java/com/unipampa/imobiliaria/interfaces/service/IPropertyService.java
@@ -0,0 +1,9 @@
+package com.unipampa.imobiliaria.interfaces.service;
+
+import com.unipampa.imobiliaria.model.Property;
+
+public interface IPropertyService {
+	
+	Property saveProperty(Property property);
+	
+}
