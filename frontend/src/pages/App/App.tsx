19005efaa5623e0d756a4e3bcd5519ccba20ab6c
Lucas Fell <lucasafell@gmail.com>
2021-02-16 21:54:24 -0300

    tailwind, folder structure

---
+++ frontend/src/pages/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-indigo-600">
+      Hello World RPIV!!
+    </div>
+  );
+}
+
+export default App;
